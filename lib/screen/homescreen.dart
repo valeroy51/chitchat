f4465cea3df0e00823dded1655a297f91ee06235
Valeroy <valeroy51@gmail.com>
2023-11-04 18:11:43 +0700

    menambahkan halaman utama, dan icon

---
+++ lib/screen/homescreen.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class homeScreen extends StatefulWidget {
+  const homeScreen({super.key});
+
+  @override
+  State<homeScreen> createState() => _homeScreenState();
+}
+
+class _homeScreenState extends State<homeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: const Icon(Icons.home),
+        title: const Text('ChitChat'),
+        actions: [
+          IconButton(onPressed: () {}, icon: const Icon(Icons.search)),
+          IconButton(onPressed: () {}, icon: const Icon(Icons.more_vert))
+        ],
+      ),
+      floatingActionButton: Padding(
+        padding: const EdgeInsets.only(bottom: 10),
+        child: FloatingActionButton(
+          onPressed: () {},
+          child: const Icon(Icons.add_comment_rounded),
+        ),
+      ),
+    );
+  }
+}
