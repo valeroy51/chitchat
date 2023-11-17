4163d1c173c790f2847ad8166e4456abbc87dccb
Valeroy <valeroy51@gmail.com>
2023-11-17 10:04:55 +0700

    menambahkan storag firebase dan depedencies untuk mengupload

--- lib/api/api.dart
+++ lib/api/api.dart
@@ -1,11 +1,14 @@
 import 'package:chitchat/models/chat_user.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 
 class apis {
   static FirebaseAuth auth = FirebaseAuth.instance;
 
   static FirebaseFirestore firestore = FirebaseFirestore.instance;
+  
+  static FirebaseStorage storage = FirebaseStorage.instance;
 
   static late ChatUser me;
 
