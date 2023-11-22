7d96fa9fa2d1727202330dfb8cd1c1a1252837e9
Stefanus Anthony <kangkung@gmail.com>
2023-11-22 10:43:36 +0700

    notif masuk

--- lib/api/api.dart
+++ lib/api/api.dart
@@ -1,3 +1,4 @@
+import 'dart:convert';
 import 'dart:developer';
 import 'dart:io';
 
@@ -7,6 +8,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:firebase_storage/firebase_storage.dart';
+import 'package:http/http.dart';
 
 class apis {
   static FirebaseAuth auth = FirebaseAuth.instance;
@@ -30,6 +32,32 @@ class apis {
         log('Push Token:$t');
       }
     });
+  }
+
+  static Future<void> sendPushNotification(
+      ChatUser chatUser, String msg) async {
+    try {
+      final body = {
+        "to": chatUser.PushToken,
+        "notification": {
+          "title": chatUser.Name, //our name should be send
+          "body": msg,
+          "android_channel_id": "chats"
+        }
+      };
+
+      var res = await post(Uri.parse('https://fcm.googleapis.com/fcm/send'),
+          headers: {
+            HttpHeaders.contentTypeHeader: 'application/json',
+            HttpHeaders.authorizationHeader:
+                'key = AAAAwoCwmFo:APA91bHanLhFFsNpOJMi78whHdQJus7MGF_-7SIn1uTG9AvnQcuSYbWT4r77Bjhup8Kc69pap3yif4N_PdEg4zghGKA9IwoT7Noo4c__ZQQ65RHa6d3P-bTa5mcebKKrJ39Q0RKIJnCD'
+          },
+          body: jsonEncode(body));
+      log('Response status: ${res.statusCode}');
+      log('Response body: ${res.body}');
+    } catch (e) {
+      log('\nsendPushNotificationE: $e');
+    }
   }
 
   static Future<bool> userExists() async {
@@ -142,7 +170,8 @@ class apis {
 
     final ref = firestore
         .collection('Chats/${getConversationID(chatUser.Id)}/Messages/');
-    await ref.doc(time).set(messages.toJson());
+    await ref.doc(time).set(messages.toJson()).then((value) =>
+        sendPushNotification(chatUser, type == Type.text ? msg : 'image'));
   }
 
   static Future<void> updateMessageReadStatus(Messages messages) async {
