ed21c9b1cbf0d3ca07204a2295cafe3b9b98b7ef
Stefanus Anthony <kangkung@gmail.com>
2023-11-24 12:13:33 +0700

    coba

---
+++ lib/screen/cobascreen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Cobascreen extends StatelessWidget {
+  const Cobascreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("CobaScreen"),
+      ),
+    );
+  }
+}
