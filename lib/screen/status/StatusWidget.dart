0db809bec825b7d5b8de6eb3954436c764a153e7
Stefanus Anthony <kangkung@gmail.com>
2023-12-03 12:52:29 +0700

    status dan navbar

--- lib/screen/status/StatusWidget.dart
+++ lib/screen/status/StatusWidget.dart
@@ -55,7 +55,7 @@ class _StatusWidgetState extends State<StatusWidget> {
                   child: ClipRRect(
                     borderRadius: BorderRadius.circular(40),
                     child: Image.asset(
-                      "img/status1.jpg",
+                      "img/logo12.png",
                       height: 55,
                       width: 55,
                       fit: BoxFit.cover,
@@ -68,7 +68,7 @@ class _StatusWidgetState extends State<StatusWidget> {
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
                       Text(
-                        "Me",
+                        "Chitchat",
                         style: TextStyle(
                             fontSize: 18, fontWeight: FontWeight.bold),
                       ),
@@ -101,129 +101,127 @@ class _StatusWidgetState extends State<StatusWidget> {
             Container(
               alignment: Alignment.centerLeft,
               child: Text(
-                "Favourite Friends",
+                "Updated",
                 style: TextStyle(
                     fontWeight: FontWeight.w500,
                     fontSize: 16,
                     color: Colors.black.withOpacity(0.6)),
               ),
             ),
-            for (int i = 1; i <= 3; i++)
-              Container(
-                margin: EdgeInsets.symmetric(vertical: 12),
-                child: Row(
-                  children: [
-                    GestureDetector(
-                      onTap: () => Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => StoryPageView())),
-                      child: Container(
-                        padding: EdgeInsets.all(1.5),
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(40),
-                            border: Border.all(color: Colors.blue, width: 3)),
-                        child: ClipRRect(
+            Container(
+              margin: EdgeInsets.symmetric(vertical: 12),
+              child: Row(
+                children: [
+                  GestureDetector(
+                    onTap: () => Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => StoryPageView())),
+                    child: Container(
+                      padding: EdgeInsets.all(1.5),
+                      decoration: BoxDecoration(
                           borderRadius: BorderRadius.circular(40),
-                          child: Image.asset(
-                            "img/status$i.jpeg",
-                            height: 55,
-                            width: 55,
-                            fit: BoxFit.cover,
-                          ),
+                          border: Border.all(color: Colors.blue, width: 3)),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(40),
+                        child: Image.asset(
+                          "img/roy.png",
+                          height: 55,
+                          width: 55,
+                          fit: BoxFit.cover,
                         ),
                       ),
                     ),
-                    Padding(
-                      padding: EdgeInsets.only(left: 20),
-                      child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              "Connie",
-                              style: TextStyle(
-                                fontSize: 18,
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(left: 20),
+                    child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "Connie",
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                          SizedBox(
+                            height: 8,
+                          ),
+                          Text(
+                            "Today, 2:00",
+                            style: TextStyle(
+                                fontSize: 15,
                                 fontWeight: FontWeight.w500,
-                              ),
-                            ),
-                            SizedBox(
-                              height: 8,
-                            ),
-                            Text(
-                              "Today, 2:00",
-                              style: TextStyle(
-                                  fontSize: 15,
-                                  fontWeight: FontWeight.w500,
-                                  color: Colors.black54),
-                            )
-                          ]),
-                    ),
-                  ],
-                ),
+                                color: Colors.black54),
+                          )
+                        ]),
+                  ),
+                ],
               ),
+            ),
             Container(
               alignment: Alignment.centerLeft,
               child: Text(
-                "Friends",
+                "Viewed",
                 style: TextStyle(
                     fontWeight: FontWeight.w500,
                     fontSize: 16,
                     color: Colors.black.withOpacity(0.6)),
               ),
             ),
-            for (int i = 4; i <= 5; i++)
-              Container(
-                margin: EdgeInsets.symmetric(vertical: 12),
-                child: Row(
-                  children: [
-                    GestureDetector(
-                      onTap: () => Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => StoryPageViewed())),
-                      child: Container(
-                        padding: EdgeInsets.all(1.5),
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(40),
-                            border: Border.all(color: Colors.grey, width: 3)),
-                        child: ClipRRect(
+            Container(
+              margin: EdgeInsets.symmetric(vertical: 12),
+              child: Row(
+                children: [
+                  GestureDetector(
+                    onTap: () => Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => StoryPageViewed())),
+                    child: Container(
+                      padding: EdgeInsets.all(1.5),
+                      decoration: BoxDecoration(
                           borderRadius: BorderRadius.circular(40),
-                          child: Image.asset(
-                            "img/status$i.jpg",
-                            height: 55,
-                            width: 55,
-                            fit: BoxFit.cover,
-                          ),
+                          border: Border.all(color: Colors.grey, width: 3)),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(40),
+                        child: Image.asset(
+                          "img/benaya.png",
+                          height: 55,
+                          width: 55,
+                          fit: BoxFit.cover,
                         ),
                       ),
                     ),
-                    Padding(
-                      padding: EdgeInsets.only(left: 20),
-                      child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              "Connie",
-                              style: TextStyle(
-                                fontSize: 18,
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(left: 20),
+                    child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "Benaya",
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                          SizedBox(
+                            height: 8,
+                          ),
+                          Text(
+                            "Today, 12:00",
+                            style: TextStyle(
+                                fontSize: 15,
                                 fontWeight: FontWeight.w500,
-                              ),
-                            ),
-                            SizedBox(
-                              height: 8,
-                            ),
-                            Text(
-                              "Today, 12:00",
-                              style: TextStyle(
-                                  fontSize: 15,
-                                  fontWeight: FontWeight.w500,
-                                  color: Colors.black54),
-                            )
-                          ]),
-                    ),
-                  ],
-                ),
-              )
+                                color: Colors.black54),
+                          )
+                        ]),
+                  ),
+                ],
+              ),
+            )
           ]),
         ),
       ),
